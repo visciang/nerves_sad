3a4ecb4f9bcad875ee2ba510a3fd1545d7ed898c
Giovanni Visciano <giovanni_visciano@yahoo.it>
2018-01-31 20:16:41 +0100

    bump app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NervesSAD.Mixfile do
   def project do
     [
       app: :nerves_sad,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
